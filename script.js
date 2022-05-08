54a7df6e96365d405a3a450b29ed3b208f58ae6e
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-05-08 15:31:38 -0600

    connected js files to html and accessed api

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// $.ajax({url:'https://rws-cards-api.herokuapp.com/api/v1/cards'}).then((date) => {
-//     console.log(data)
-// },
+const URL = 'https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3'
+$.ajax(URL).then(function(data) {
+    console.log(data)
+})
 // (error) => {
 //     console.log('bad request', error);
 // });
 
 // let cards;
 
-//connect the api 
-// 
+//connect the api with AJAX
+//create event listener for button
+//deal cards on click
+//show images for 3 cards
+//link api definitions 
+//show definitions on hover
+
