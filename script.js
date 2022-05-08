d540c32f8108d251d20bdc6c01bdb8c2f4b47a29
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-05-08 14:19:57 -0600

    added title, deal button, background img, and js psuedo code.

---
+++ script.js
@@ -0,0 +1,11 @@
+// $.ajax({url:'https://rws-cards-api.herokuapp.com/api/v1/cards'}).then((date) => {
+//     console.log(data)
+// },
+// (error) => {
+//     console.log('bad request', error);
+// });
+
+// let cards;
+
+//connect the api 
+// 
