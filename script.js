44c0e2b005bd9387601de9feab7d942784342341
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-05-09 14:33:47 -0600

    added functions for 2nd and 3rd card, started initial CSS

--- script.js
+++ script.js
@@ -13,18 +13,34 @@ function showDeck() {
             const firstCard = data.cards[0].meaning_up
             const secondCard = data.cards[1].meaning_up
             const thirdCard = data.cards[2].meaning_up
-        console.log(firstCard)
-        console.log(secondCard)
-        console.log(thirdCard)
+        // console.log(firstCard)
+        // console.log(secondCard)
+        // console.log(thirdCard)
         displayFirst(firstCard)
+        displaySecond(secondCard)
+        displayThird(thirdCard)
           }) 
         }
 function displayFirst(firstCard) {
-  const $test = $('<h2>')
-  $test.text(firstCard)
-  $mainContent.append($test)
+  const $cardPast = $('<div>')
+  $cardPast.addClass('past')
+  $cardPast.text(firstCard)
+  $mainContent.append($cardPast)
 }
-       
+
+function displaySecond(secondCard) {
+  const $cardPresent = $('<div>')
+  $cardPresent.addClass('present')
+  $cardPresent.text(secondCard)
+  $mainContent.append($cardPresent)
+}
+
+function displayThird(thirdCard) {
+  const $cardFuture = $('<div>')
+  $cardFuture.addClass('future')
+  $cardFuture.text(thirdCard)
+  $mainContent.append($cardFuture)
+}   
 // Event Listener 
 const $deal = $('#btn').on('click', showDeck) 
 
